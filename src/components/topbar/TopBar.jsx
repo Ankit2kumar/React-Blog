73de947d3a425c1294ba2dcec884461e32dfe4b6
Ankit Kumar <ankitlucky.kumar99@gmail.com>
2021-11-20 18:29:14 +0100

    adds routes

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -12,7 +12,9 @@ export default function TopBar() {
 			</div>
 			<div className="topCenter">
 				<ul className="topList">
-					<li className="topListItem">HOME</li>
+					<li className="topListItem">
+						<Link to="/">HOME </Link>
+					</li>
 					<li className="topListItem">ABOUT</li>
 					<li className="topListItem">CONTACT</li>
 					<li className="topListItem">
